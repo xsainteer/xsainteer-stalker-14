0111c4891687785c77516679c6f4b6d73789416c
Konstantin <bormotovk@gmail.com>
2024-09-23 17:35:26 +0200

    Fixes

---
+++ Content.Shared/_Stalker/Mobs/CritEvents.cs
@@ -0,0 +1,12 @@
+using Content.Shared.Actions;
+
+namespace Content.Shared._Stalker.Mobs;
+
+public sealed partial class CritGiveUp : InstantActionEvent
+{
+}
+
+public sealed partial class CritFightForLife : InstantActionEvent
+{
+}
+

950c1bb2c58984eed0afb63bd96ca8beb37368e9
Konstantin <bormotovk@gmail.com>
2024-10-06 12:25:59 +0200

    Dizzy Overlay fixed. Seraphims got psy.

--- Content.Client/_Stalker/Dizzy/DizzyOverlay.cs
+++ Content.Client/_Stalker/Dizzy/DizzyOverlay.cs
@@ -24,7 +24,7 @@ public sealed class DizzyOverlay : Overlay
     public DizzyOverlay()
     {
         IoCManager.InjectDependencies(this);
-        _dizzyShader = _prototypeManager.Index<ShaderPrototype>("Drunk").InstanceUnique();
+        _dizzyShader = _prototypeManager.Index<ShaderPrototype>("STDizzy").InstanceUnique();
     }
 
     public void Stop()

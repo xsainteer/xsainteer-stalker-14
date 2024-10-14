8a1adc2e4d265ea40789a1564a64b69333c497e8
киригай <81517221+Kiriyaga7615@users.noreply.github.com>
2024-10-14 23:47:14 +0200

    Update PuddleSystem.Evaporation.cs

--- Content.Server/Fluids/EntitySystems/PuddleSystem.Evaporation.cs
+++ Content.Server/Fluids/EntitySystems/PuddleSystem.Evaporation.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Chemistry.Components;
+using Content.Shared.Chemistry.Reagent; // stalker-changes
 using Content.Shared.FixedPoint;
 using Content.Shared.Fluids.Components;
 
@@ -8,7 +9,20 @@ public sealed partial class PuddleSystem
 {
     private static readonly TimeSpan EvaporationCooldown = TimeSpan.FromSeconds(1);
 
-    private void OnEvaporationMapInit(Entity<EvaporationComponent> entity, ref MapInitEvent args)
+    // stalker-changes-start
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string Water = "Water";
+
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string STWater = "STWater";
+
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string Vodka = "Vodka";
+
+    public static string[] EvaporationReagents = new[] { Water, Blood, Vodka, STWater};
+
+    //stalker-changes-end
+    private void OnEvaporationMapInit(Entity<EvaporationComponent> entity, ref MapInitEvent args) // stalker-changes
     {
         entity.Comp.NextTick = _timing.CurTime + EvaporationCooldown;
     }

5fadaec5b4adc9e740a082bbdc99603ad87df68b
Jerry <48524572+JerryImMouse@users.noreply.github.com>
2025-02-13 23:01:47 +0500

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -7,7 +7,7 @@ from typing import Iterable
 
 PUBLISH_TOKEN = os.environ["PUBLISH_TOKEN"]
 VERSION = os.environ["GITHUB_SHA"]
-
+FORK_ID = os.environ["FORK_ID"]
 RELEASE_DIR = "release"
 
 #
@@ -15,7 +15,6 @@ RELEASE_DIR = "release"
 # Forks should change these to publish to their own infrastructure.
 #
 ROBUST_CDN_URL = "https://cdn.corvaxforge.ru/" # TODO: Point stalkers14.xyz to the same IP
-FORK_ID = "stalker"
 
 def main():
     session = requests.Session()

d0fc867ef04df790ca4f65738821bcaaa5df918b
LordVladimer <valdis_fedorov@mail.ru>
2025-03-18 13:33:22 +0300

    Revert "Постовые и приказы СОП"
    This reverts commit 04deaf1bc5c6d0e48c0327d43384289d08ef3769.

--- Content.Shared/_Stalker/GuradFactionController/GuardFactionControllerEvents.cs
+++
@@ -1,7 +0,0 @@
-using Content.Shared.Actions;
-
-namespace Content.Shared._Stalker.GuardFactionController // Changed
-{
-    public sealed partial class GuardFactionControllerOffActionEvent : InstantActionEvent { } // Changed
-    public sealed partial class GuardFactionControllerOnActionEvent : InstantActionEvent { } // Changed
-}

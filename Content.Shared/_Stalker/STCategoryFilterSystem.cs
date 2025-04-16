5af92f6b0883f4b24c4ab507a0623ce8ffccfc13
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2025-04-16 23:30:16 +1000

    feat: add ForkFiltered system

---
+++ Content.Shared/_Stalker/STCategoryFilterSystem.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._Stalker;
+
+public sealed class STCategoryFilterSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    public override void Initialize()
+    {
+        _cfg.SetCVar(CVars.EntitiesCategoryFilter, "ForkFiltered");
+    }
+}

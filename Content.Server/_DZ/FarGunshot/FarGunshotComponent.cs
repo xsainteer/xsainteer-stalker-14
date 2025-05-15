b2448c98c0bddb8f384d06175f91a19dc46bce49
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-05-15 11:16:18 +0200

    fargunshots base

---
+++ Content.Server/_DZ/FarGunshot/FarGunshotComponent.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Tools.Components;
+using Robust.Shared.Audio;
+
+namespace Content.Server._DZ.FarGunshot;
+
+[RegisterComponent]
+public sealed partial class FarGunshotComponent : Component
+{
+    /// <summary>
+    /// The start max range of farsound, non modified
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float Range = 260;
+
+    /// <summary>
+    /// The base sound to use when the gun is fired from far dist.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier? Sound = new SoundPathSpecifier("/Audio/_DZ/Weapons/Guns/Gunshots/gunshot-far.ogg");
+
+    // TODO: silencer impl
+}
---
+++ Content.Server/_DZ/FarGunshot/FarGunshotSystem.cs
@@ -0,0 +1,48 @@
+﻿using Content.Shared.Coordinates;
+using Content.Shared.Weapons.Ranged.Events;
+using Robust.Server.GameObjects;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Player;
+
+namespace Content.Server._DZ.FarGunshot;
+
+public sealed class FarGunshotSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly TransformSystem _transform = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<FarGunshotComponent, AmmoShotEvent>(OnFarGunshot);
+
+    }
+
+    public void OnFarGunshot(EntityUid uid, FarGunshotComponent component, AmmoShotEvent args)
+    {
+
+        if (uid == EntityUid.Invalid || component.Range <= 14f)
+            return;
+
+        var shootPos = _transform.GetMapCoordinates(uid);
+
+        // Create a filter for players who are far enough to hear the distant gunshot,
+        // excluding those within close range (14)
+        var farSoundFilter = Filter.Empty()
+            .AddInRange(shootPos, component.Range)
+            .RemoveInRange(shootPos, 14f);
+
+        // Play the distant gunshot sound globally:
+        // - Enabled for replay recording
+        // - Not toggleable by the player (forced playback)
+        _audio.PlayGlobal(
+            component.Sound,
+            farSoundFilter,
+            recordReplay: true,
+            AudioParams.Default
+        );
+    }
+
+}

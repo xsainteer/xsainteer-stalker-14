d17791dd423eb94016f519435e0126e6e29368af
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-05-30 22:49:32 +1000

    feat: status system

---
+++ Content.Shared/_RD/StatusEffect/Components/RDStatusEffectComponent.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Project: raincidation
+ * File: RDStatusEffectComponent.cs
+ * License: All rights reserved
+ * Copyright: (c) 2025 TornadoTechnology
+ *
+ * For the full license text, see the LICENSE file in the project root.
+ * Link: https://github.com/Rainlucid/raincidation
+ */
+
+using Content.Shared._RD.StatusEffect.Systems;
+using Content.Shared.Alert;
+using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared._RD.StatusEffect.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, AutoGenerateComponentPause]
+[Access(typeof(RDStatusEffectSystem))]
+public sealed partial class RDStatusEffectComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public EntityUid? AppliedTo;
+
+    [DataField, AutoNetworkedField]
+    public ProtoId<AlertPrototype>? Alert;
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField, AutoNetworkedField]
+    public TimeSpan? EndEffectTime;
+
+    [DataField]
+    public EntityWhitelist? Whitelist;
+
+    [DataField]
+    public EntityWhitelist? Blacklist;
+
+    [DataField]
+    public ComponentRegistry Components = new();
+}
---
+++ Content.Shared/_RD/StatusEffect/Events/RDStatusEffectAppliedEvent.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Project: raincidation
+ * File: RDStatusEffectAppliedEvent.cs
+ * License: All rights reserved
+ * Copyright: (c) 2025 TornadoTechnology
+ *
+ * For the full license text, see the LICENSE file in the project root.
+ * Link: https://github.com/Rainlucid/raincidation
+ */
+
+namespace Content.Shared._RD.StatusEffect.Events;
+
+/// <summary>
+/// Calls on both effect entity and target entity, when a status effect is applied.
+/// </summary>
+[ByRefEvent]
+public readonly record struct RDStatusEffectAppliedEvent(EntityUid Target, Entity<Components.RDStatusEffectComponent> Effect);

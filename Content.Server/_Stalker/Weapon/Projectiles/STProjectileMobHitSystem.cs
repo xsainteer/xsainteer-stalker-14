06892d02fc904def3606e2122dbd6593dd9fe31a
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-16 00:09:39 +1000

    fix

---
+++ Content.Server/_Stalker/Weapon/Projectiles/STProjectileMobHitSystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Mobs.Components;
+using Robust.Shared.Physics.Events;
+
+namespace Content.Server._Stalker.Weapon.Projectiles;
+
+public sealed class STProjectileMobHitSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<STProjectileMobHitComponent, PreventCollideEvent>(OnHit);
+    }
+
+    private void OnHit(Entity<STProjectileMobHitComponent> entity, ref PreventCollideEvent args)
+    {
+        if (HasComp<MobStateComponent>(args.OtherEntity))
+            return;
+
+        args.Cancelled = true;
+    }
+}

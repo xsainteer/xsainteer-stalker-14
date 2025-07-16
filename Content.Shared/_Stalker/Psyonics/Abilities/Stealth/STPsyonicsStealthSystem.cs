b200b27a8e75ae5d4b0816bd3580e642e8959816
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-16 18:07:20 +1000

    feat: stealth

---
+++ Content.Shared/_Stalker/Psyonics/Abilities/Stealth/STPsyonicsStealthSystem.cs
@@ -0,0 +1,52 @@
+﻿using Content.Shared._Stalker.Invisibility;
+using Content.Shared.Actions;
+
+namespace Content.Shared._Stalker.Psyonics.Abilities.Stealth;
+
+public sealed class STPsyonicsStealthSystem : EntitySystem
+{
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<STPsyonicsStealthComponent, ComponentStartup>(OnStartup);
+        SubscribeLocalEvent<STPsyonicsStealthComponent, ComponentShutdown>(OnShutdown);
+        SubscribeLocalEvent<STPsyonicsStealthComponent, STPsyonicsStealthActionEvent>(OnAction);
+    }
+
+    private void OnStartup(Entity<STPsyonicsStealthComponent> entity, ref ComponentStartup args)
+    {
+        entity.Comp.Action = _actions.AddAction(entity, entity.Comp.ActionId);
+        Dirty(entity);
+    }
+
+    private void OnShutdown(Entity<STPsyonicsStealthComponent> entity, ref ComponentShutdown args)
+    {
+        if (entity.Comp.Action is null)
+            return;
+
+        _actions.RemoveAction(entity, entity.Comp.Action.Value);
+
+        entity.Comp.Action = null;
+        Dirty(entity);
+    }
+
+    private void OnAction(Entity<STPsyonicsStealthComponent> entity, ref STPsyonicsStealthActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = true;
+
+        if (EnsureComp<STInvisibleComponent>(entity, out var invisibleComponent))
+        {
+            RemCompDeferred<STInvisibleComponent>(entity);
+            return;
+        }
+
+        invisibleComponent.Opacity = entity.Comp.Opacity;
+        Dirty(entity, invisibleComponent);
+    }
+}

1e86c1fcf26656cee22133266c7056427480057f
xsainteer <makmatov123@gmail.com>
2025-08-02 17:37:24 +0600

    part 2

---
+++ Content.Shared/_Stalker/ZoneAlert/ZoneGradationTriggerComponent.cs
@@ -0,0 +1,15 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Stalker.ZoneAlert;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ZoneGradationTriggerComponent : Component
+{
+    /// <summary>
+    /// What gradation this trigger is set to.
+    /// </summary>
+    public ZoneGradation ZoneGradation = ZoneGradation.GreenZone;
+}
